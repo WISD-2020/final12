e77d3ca49e39489b2e85754f3a2ee93ce207d42e
廖韋臣 <rax02270521@gmail.com>
2020-12-22 21:49:59 +0800

    初始模板整理套用

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/heroic-features.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="container">
+@include('frontend.layouts.header')
+@include('frontend.layouts.navbar')
+@yield('content')
+</div>
+@include('frontend.layouts.footer')
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('jquery/jquery.min.js')}}"></script>
+<script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('/',function (){
+   return view('frontend.index');
+})->name('home');
