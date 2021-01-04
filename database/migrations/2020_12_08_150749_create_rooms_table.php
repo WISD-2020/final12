0f72689fc4064241c4432645c2e1912b0493f5a7
廖韋臣 <rax02270521@gmail.com>
2021-01-04 18:11:21 +0800

    修改rooms資料表

--- database/migrations/2020_12_08_150749_create_rooms_table.php
+++ database/migrations/2020_12_08_150749_create_rooms_table.php
@@ -14,14 +14,12 @@ class CreateRoomsTable extends Migration
     public function up()
     {
         Schema::create('rooms', function (Blueprint $table) {
-            $table->primary('id',20)->comment('G一般 L加大 I 單人 D雙人 例如:GI1004 一般單人套房一樓房號004');
-
+            $table->id('id')->comment('G一般 L加大 I 單人 D雙人 例如:GI1004 一般單人套房一樓房號004');
             $table->string('room_type',20);
             $table->string('room_address',50);
             $table->integer('room_price');
             $table->string('room_photo');
             $table->timestamps();
-
         });
     }
 
