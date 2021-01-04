9d32bef03847f201eb8ec7e61305bbf8f0dd1915
廖韋臣 <rax02270521@gmail.com>
2021-01-04 20:27:53 +0800

    費用SQL

--- app/Http/Controllers/CostController.php
+++ app/Http/Controllers/CostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cost;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CostController extends Controller
 {
@@ -15,10 +16,15 @@ class CostController extends Controller
      */
     public function index()
     {
+      $costs=DB::table('costs')
+          ->where('costs.room_id','=',auth()->user()->room_id)
+          ->join('users','costs.room_id','=','users.room_id')
+          ->select('costs.room_id','waterbill','electricitybill','consumption','public_e',
+                           'rent','w_status','e_status','r_status','cost_month','name',)
+          ->get();
 
 
-
-        return view('tenant.cost');
+        return view('tenant.cost',['costs'=>$costs]);
 
     }
     public function admin_index()
