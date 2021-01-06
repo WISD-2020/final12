e76aa42d0f0b52d95afdeb257abbeb4e49b328f3
廖韋臣 <rax02270521@gmail.com>
2021-01-06 10:19:22 +0800

    房客修繕回報路由設置

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,14 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('cost', [CostController::class, 'index'])->name('cost.index');
     Route::get('mail', [MailController::class, 'index'])->name('mail.index');
-    Route::get('repair', [RepairController::class, 'index'])->name('repair.index');
     Route::get('users_show',[UserController::class,'show'])->name('users_show.index');
+    /*房客修繕回報*/
+    Route::get('repair', [RepairController::class, 'index'])->name('repair.index');
+    Route::get('repair/create',[RepairController::class,'create'])->name('repair.create');
+    Route::post('repair/store',[RepairController::class,'store'])->name('repair.store');
+    Route::get('repair/{id}/edit',[RepairController::class,'edit'])->name('repair.edit');
+    Route::patch('repair/{id}',[RepairController::class,'update'])->name('repair.update');
+    Route::delete('repair/{id}',[RepairController::class,'destroy'])->name('repair.destroy');
 });
 
 
